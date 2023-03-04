043038ff5c524471c8548c77f6361a0c396af7ee
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2023-03-04 09:08:47 +0100

    add showBooks and book cards and buttons to Boos.js

--- src/features/books/Book.js
+++ src/features/books/Book.js
@@ -4,10 +4,26 @@ import { useSelector } from 'react-redux';
 const Book = () => {
 
   const { books } = useSelector((state) => (state.booksR));
-  alert(JSON.stringify(books, null, 4));
+  // alert(JSON.stringify(books, null, 4));
+  const renderBooksElement = books.map((book) => {
+    const { id, title, author } = book;
+    return (
+      <div key={id} className='showBooks'>
+        <h3>{title}</h3>
+        <h3>{author}</h3>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    )
+  });
+
   return (
     <div>
-      <h2>List of Books</h2>
+      <h2 style={{ marginLeft: '40rem' }}>List of Books</h2>
+      <div className='booksCollection'>
+        {renderBooksElement}
+      </div>
+
     </div>
   )
 }
